350b244ef35c0f693b545e3bb1ec652b1e08f3d7
tisegenene <tisegenene@gmail.com>
2025-04-15 15:09:04 +0300

    Fix: Fixed Everything

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -1,12 +1,10 @@
-// src/app/page.tsx
-import { fetchScholarships } from './lib/strapi';
+import "./globals.css";
 
-export default async function Home() {
-  const scholarships = await fetchScholarships();
-
+const Test = () => {
   return (
-    <div className="p-4 max-w-4xl mx-auto">
-      <h1 className="text-3xl font-bold mb-6">Landing Page Coming Soon</h1>
-    </div>
+	<h1 className="text-3xl text-white font-bold mb-6">Landing Page Coming Soon</h1>
   );
-}
+};
+
+export default Test;
+
